fe4cfa83b898b32246a3c49b932ab484cfffe603
Rex Chan <rexchan@Apples-MacBook-Pro.local>
2024-11-22 21:48:56 +0800

    Add button Done

--- todolist/app/page.js
+++ todolist/app/page.js
@@ -1,8 +1,13 @@
 'use client'
 
 import Image from "next/image";
+import {useState, useRef} from 'react' 
 
 export default function Home() {
+
+  const [toDoItem, setToDoItem] = useState([])
+  const NewTask = useRef('')
+
   return <>
     <div className="custom-gradient h-[100vh] w-full flex justify-center items-center">
       <div className="w-[400px] h-[200px] rounded-lg bg-[#ffffff] flex justify-center ">
@@ -15,11 +20,19 @@ export default function Home() {
           </div>
           <div className="flex items-center justify-center bg-[#edeef0] rounded-[30px] pl-[20px] mb-[25px] mt-[10px] h-[30px]">
             <input
+              onChange={(e)=>{
+                NewTask.current = e.target.value
+              }}
               type="text"
               placeholder="Add your text"
               className="flex-1 border-none outline-none bg-transparent p-[10px] text-[14px] text-black"
             />
-            <button className="border-none outline-none px-[50px] bg-[#ff5945] text-white text-[16px] cursor-pointer rounded-[40px] h-[30px]">
+            <button className="border-none outline-none px-[50px] bg-[#ff5945] text-white text-[16px] cursor-pointer rounded-[40px] h-[30px]"
+              onClick={()=>{
+                setToDoItem((prev)=>{
+                  return [...prev,{task:NewTask.current, completed: false}]})
+              }}  
+            >
               Add
             </button>
           </div>
